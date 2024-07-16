ddca57427e54b68d3fd8fd0f4ed64bd594a57589
Jieun Oh <ohjieun@kakao.com>
2024-07-16 16:13:08 +0900

    Chore: font 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", ...defaultTheme.fontFamily.sans],
+        roboto: ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
 };
