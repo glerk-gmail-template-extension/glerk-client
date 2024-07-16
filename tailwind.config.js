385fb1d5c20625da10d21e720f39e0ef9c728f74
Jieun Oh <ohjieun@kakao.com>
2024-07-16 16:18:56 +0900

    Design: tailwind config custom theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,27 @@ export default {
         sans: ["Pretendard", ...defaultTheme.fontFamily.sans],
         roboto: ["Roboto Slab", "serif"],
       },
+      colors: {
+        primary: "#2c9f73",
+        stroke: "#eaecf0",
+        black: "#0f1012",
+        blue: "#5089f9",
+        yellow: "#f7c349",
+        darkGray: "#3c4043",
+        lightGray: "#c3c3c6",
+        red: "#f95050",
+        purple: "#604cc3",
+        "ghost-white": "#f4f6fc",
+      },
+      width: {
+        100: "25rem",
+      },
+      height: {
+        120: "30rem",
+      },
+      boxShadow: {
+        "3xl": "8px 8px 16px 0 rgba(0, 0, 0, 0.08)",
+      },
     },
   },
   plugins: [],
