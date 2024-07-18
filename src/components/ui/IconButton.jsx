e18b26e2560f82a9ae43750fe40a796db7ea7f05
Jieun Oh <ohjieun@kakao.com>
2024-07-19 01:59:40 +0900

    Fix: 툴팁 조건부 렌더링 및 onClick 핸들러 추가

--- src/components/ui/IconButton.jsx
+++ src/components/ui/IconButton.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
-export default function IconButton({ children, tooltip }) {
+export default function IconButton({ children, tooltip, onClick = () => {} }) {
   const [showTooltip, setShowTooltip] = useState(false);
 
   return (
-    <div className="relative">
+    <div className="relative inline-block">
       <button
+        type="button"
         className="flex items-center justify-center w-8 h-8 rounded-full hover:bg-gray-100"
+        onClick={onClick}
         onMouseEnter={() => setShowTooltip(true)}
         onMouseLeave={() => setShowTooltip(false)}
       >
         {children}
       </button>
-      {showTooltip && (
+      {showTooltip && tooltip && (
         <div className="absolute px-2 py-1 mb-2 text-xs text-white transform -translate-x-1/2 bg-black rounded-md bottom-full left-1/2 whitespace-nowrap w-max">
           {tooltip}
         </div>
