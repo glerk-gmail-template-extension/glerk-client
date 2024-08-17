0f8bc63682fd34b83d6a76c8d683be64307d3f68
Jieun Oh <ohjieun@kakao.com>
2024-08-17 14:36:53 +0900

    Layout: Footer 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import Navigation from "./components/navigation/Navigation";
+import Footer from "./components/footer/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-1 p-10 mb-20">
+      <main className="flex-1 h-auto min-h-full p-10 mb-20">
         <div className="mx-auto min-w-168 max-w-240">
           <Navigation />
           <section className="mt-12">
@@ -15,6 +16,7 @@ function App() {
           </section>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="relative h-10 py-2 text-sm text-center text-gray-400 -translate-y-full">
+      <p>Glerk</p>
+      <p>Copyright 2024. All Rights Reserved.</p>
+    </footer>
+  );
+}
